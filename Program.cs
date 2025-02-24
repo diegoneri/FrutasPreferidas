88db89b810fe34b126dd5682a263e25e430d8a54
Diego Neri <diego@diegoneri.com>
2025-02-24 11:04:47 -0300

    Finaliza programa com as 5 frutas favoritas

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("########################");
+Console.WriteLine("### Frutas Favoritas ###");
+Console.WriteLine("########################");
+
+Console.WriteLine();
+
+Console.Write("1. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Melancia");
+Console.ResetColor();
+
+Console.Write("2. ");
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Uva");
+Console.ResetColor();
+
+Console.Write("3. ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.BackgroundColor = ConsoleColor.Black;
+Console.WriteLine("Laranja");
+Console.ResetColor();
+
+Console.Write("4. ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.BackgroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Pitaya");
+Console.ResetColor();
+
+Console.Write("5. ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.BackgroundColor = ConsoleColor.White;
+Console.WriteLine("Limão");
+Console.ResetColor();
